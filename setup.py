2c544ec9736e6a209189311c0c67d30ded586bcd
Timothy Chandler <ernaldis+github@protonmail.com>
2022-09-27 14:06:45 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import sys
+
+pkg_name = 'workflow_calcium_imaging'
+here = path.abspath(path.dirname(__file__))
+
+long_description = """"
+# Workflow for calcium imaging data acquired with ScanImage, Scanbox, or Nikon NIS software and analyzed with Suite2p or CaImAn.
+
+Build a complete imaging workflow using the DataJoint elements
++ [element-lab](https://github.com/datajoint/element-lab)
++ [element-animal](https://github.com/datajoint/element-animal)
++ [element-session](https://github.com/datajoint/element-session)
++ [element-event](https://github.com/datajoint/element-event)
++ [element-calcium-imaging](https://github.com/datajoint/element-calcium-imaging)
+"""
+
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
+with open(path.join(here, pkg_name, 'version.py')) as f:
+    exec(f.read())
+
+setup(
+    name='workflow-calcium-imaging',
+    version=__version__,
+    description="Calcium imaging workflow using the DataJoint elements",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='DataJoint',
+    author_email='info@datajoint.com',
+    license='MIT',
+    url='https://github.com/datajoint/workflow-calcium-imaging',
+    keywords='neuroscience datajoint calcium-imaging',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=requirements,
+)
---
+++ workflow_calcium_imaging/pipeline.py
@@ -0,0 +1,78 @@
+import datajoint as dj
+from element_lab import lab
+from element_animal import subject
+from element_session import session_with_datetime as session
+from element_event import trial, event
+from element_calcium_imaging import scan, imaging
+from element_lab.lab import Source, Lab, Protocol, User, Location, Project
+from element_animal.subject import Subject
+from .paths import (
+    get_imaging_root_data_dir,
+    get_scan_image_files,
+    get_scan_box_files,
+    get_nd2_files,
+)
+from . import analysis
+
+if "custom" not in dj.config:
+    dj.config["custom"] = {}
+
+db_prefix = dj.config["custom"].get("database.prefix", "")
+
+__all__ = [
+    "subject",
+    "lab",
+    "session",
+    "Equipment",
+    "trial",
+    "event",
+    "scan",
+    "imaging",
+    "Subject",
+    "Source",
+    "Lab",
+    "Protocol",
+    "User",
+    "Project",
+    "Session",
+    "Location",
+    "get_imaging_root_data_dir",
+    "get_scan_image_files",
+    "get_scan_box_files",
+    "get_nd2_files",
+]
+
+
+# ------------- Activate "lab", "subject", "session" schema -------------
+
+lab.activate(db_prefix + "lab")
+
+subject.activate(db_prefix + "subject", linking_module=__name__)
+
+Session = session.Session
+Experimenter = lab.User
+session.activate(db_prefix + "session", linking_module=__name__)
+
+
+# Activate "event" and "trial" schema ---------------------------------
+
+trial.activate(db_prefix + "trial", db_prefix + "event", linking_module=__name__)
+
+
+# ------------- Declare table Equipment for use in element_calcium_imaging -------------
+
+
+@lab.schema
+class Equipment(dj.Manual):
+    definition = """
+    scanner: varchar(32)
+    """
+
+
+# ------------- Activate "imaging" schema -------------
+
+imaging.activate(db_prefix + "imaging", db_prefix + "scan", linking_module=__name__)
+
+# ------------- Activate "analysis" schema ------------
+
+analysis.activate(db_prefix + "analysis", linking_module=__name__)
