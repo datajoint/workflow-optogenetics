914b3d31900c9890a99f279221a2b2691b23f8d4
Kabilar Gunalan <kabilar@datajoint.com>
2023-04-18 21:49:45 +0000

    Update tests

--- tests/test_pipeline_generation.py
+++ tests/test_pipeline_generation.py
@@ -1,28 +1,29 @@
+from . import pipeline
+
+
 def test_upstream_pipeline(pipeline):
     session = pipeline["session"]
     surgery = pipeline["surgery"]
     subject = pipeline["subject"]
 
-    # test connection Subject->Session
-    assert subject.Subject.full_table_name == session.Session.parents()[0]
+    # Test connection from Subject to Session
+    assert subject.Subject.full_table_name in session.Session.parents()
     assert subject.Subject.full_table_name in surgery.Implantation.parents()
 
 
-def test_opto_pipeline(pipeline):
+def test_optogenetics_pipeline(pipeline):
     session = pipeline["session"]
     surgery = pipeline["surgery"]
-    opto = pipeline["opto"]
+    optogenetics = pipeline["optogenetics"]
     Device = pipeline["Device"]
 
-    # test connection opto.OptoProtocol -> parents
-    opto_parent_links = opto.OptoProtocol.parents()
-    opto_parent_list = [
-        session.Session,
-        opto.OptoStimParams,
-        surgery.Implantation,
-        Device,
-    ]
-    for parent in opto_parent_list:
-        assert (
-            parent.full_table_name in opto_parent_links
-        ), f"opto.OptoProtocol.parents() did not include {parent.full_table_name}"
+    # Test connection from optogenetics.OptoProtocol to parent tables
+    assert session.Session.full_table_name in optogenetics.OptoProtocol.parents()
+    assert (
+        optogenetics.OptoStimParams.full_table_name
+        in optogenetics.OptoProtocol.parents()
+    )
+    assert surgery.Implantation.full_table_name in optogenetics.OptoProtocol.parents()
+    assert Device.full_table_name in optogenetics.OptoProtocol.parents()
+
+    assert "stim_start_time" in optogenetics.OptoEvent.heading.attributes
