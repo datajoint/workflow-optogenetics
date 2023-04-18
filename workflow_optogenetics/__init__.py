5ea89305dee9e831683fa84c47a934b0b522ee37
Kabilar Gunalan <kabilar@datajoint.com>
2023-04-18 22:37:36 +0000

    Add database prefix to dj.config

--- workflow_optogenetics/__init__.py
+++ workflow_optogenetics/__init__.py
@@ -1,6 +1,12 @@
+import os
 import datajoint as dj
 
-if "custom" not in dj.config:
-    dj.config["custom"] = {}
+
+if 'custom' not in dj.config:
+    dj.config['custom'] = {}
+
+dj.config['custom']['database.prefix'] = os.getenv(
+    'DATABASE_PREFIX',
+    dj.config['custom'].get('database.prefix', ''))
 
 db_prefix = dj.config["custom"].get("database.prefix", "")
